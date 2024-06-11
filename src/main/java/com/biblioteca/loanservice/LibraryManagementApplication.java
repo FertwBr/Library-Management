02d49ea598a084b5b92eca676bc47e547b9456ce
Fernando Vaz <fertwbr@gmail.com>
2024-06-11 19:35:08 -0300

    Renomeando pacotes
    Co-Authored-By: Beatriz Gibaut <126671219+beahh0@users.noreply.github.com>
    Co-Authored-By: Lavínia Énora <121834276+enoralavinia@users.noreply.github.com>

--- src/main/java/com/biblioteca/loanservice/LibraryManagementApplication.java
+++ src/main/java/com/biblioteca/loanservice/LibraryManagementApplication.java
@@ -1,4 +1,4 @@
-package io.github.librarymanagement.librarymanagement;
+package com.biblioteca.loanservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/biblioteca/loanservice/LibraryManagementApplicationTests.java
+++ src/test/java/com/biblioteca/loanservice/LibraryManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.librarymanagement.librarymanagement;
+package com.biblioteca.loanservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
