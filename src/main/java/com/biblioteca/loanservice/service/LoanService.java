252860c080bc33f29ea85a9a60ef472151762800
Fernando Vaz <fertwbr@gmail.com>
2024-06-11 21:05:32 -0300

    feat: Implementar comunicação
    Implementar comunicação com CatalogService e NotificationService
    
    Co-Authored-By: Beatriz Gibaut <126671219+beahh0@users.noreply.github.com>
    Co-Authored-By: Lavínia Énora <121834276+enoralavinia@users.noreply.github.com>

--- src/main/java/com/biblioteca/loanservice/service/LoanService.java
+++ src/main/java/com/biblioteca/loanservice/service/LoanService.java
@@ -1,22 +1,46 @@
 package com.biblioteca.loanservice.service;
 
+import com.biblioteca.loanservice.client.CatalogServiceClient;
+import com.biblioteca.loanservice.client.NotificationServiceClient;
 import com.biblioteca.loanservice.model.Loan;
+import com.biblioteca.loanservice.model.LoanStatus;
 import com.biblioteca.loanservice.repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+// TODO verificar CatalagServiceClient e NotificationServiceClient
 @Service
 public class LoanService {
     @Autowired
     private LoanRepository loanRepository;
 
+    @Autowired
+    private CatalogServiceClient catalogServiceClient;
+
+    @Autowired
+    private NotificationServiceClient notificationServiceClient;
+
     public Loan createLoan(Loan loan) {
-        // Todo Lógica para verificar a disponibilidade do livro no CatalogService
-        // Todo Lógica para registrar o empréstimo no banco de dados
-        // Todo Lógica para notificar o usuário sobre a aprovação do empréstimo
-        return loanRepository.save(loan);
+        // Verificar a disponibilidade do livro no CatalogService
+        if (!catalogServiceClient.isBookAvailable(loan.getBookId())) {
+            loan.setStatus(LoanStatus.REJECTED);
+            notificationServiceClient.notifyLoanRejection(loan); // Notificar rejeição
+            return loanRepository.save(loan);
+        }
+
+        // Registrar o empréstimo no banco de dados
+        loan.setStatus(LoanStatus.APPROVED);
+        Loan savedLoan = loanRepository.save(loan);
+
+        // Notificar o usuário sobre a aprovação do empréstimo
+        notificationServiceClient.notifyLoanApproval(savedLoan); // Notificar aprovação
+
+        // Atualizar o status do livro para indisponível no CatalogService
+        catalogServiceClient.updateBookStatus(loan.getBookId(), false);
+
+        return savedLoan;
     }
 
     public List<Loan> getAllLoans() {
@@ -47,3 +71,7 @@ public class LoanService {
     }
 
 }
+}
+
+
+
